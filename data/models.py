1cf48799be80974e34a5c72d1ce4026ef4775de4
Yuan Yifan <tsingjyujing@163.com>
2021-01-14 23:10:33 +0900

    Add model for video and novel

--- data/models.py
+++ data/models.py
@@ -132,6 +132,105 @@ class DefaultS3Image(BaseImage):
             log.warning(f"Error while removing object: {err}")
         DefaultS3Image.objects.filter(thread=None).delete()
 
+
 # VIDEO THREAD RELATED
+class VideoThread(models.Model, ISearchable):
+
+    @classmethod
+    def get_schema(cls) -> Schema:
+        return Schema(
+            id=ID(unique=True, stored=True),
+            full_text=TEXT(analyzer=get_analyzer()),
+        )
+
+    @classmethod
+    def get_index_name(cls) -> str:
+        return "videothread"
+
+    def to_fields(self) -> Dict[str, Any]:
+        return {
+            "id": str(self.id),
+            "full_text": clean_up_chinese_str(self.title + self.description),
+        }
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    data = models.TextField(default="{}")
+    owner = models.ForeignKey(
+        "auth.User",
+        related_name="video_thread",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+
+class DefaultS3Video(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    order = models.IntegerField(default=0)
+    thread = models.ForeignKey("VideoThread", on_delete=models.SET_NULL, null=True)
+    bucket = models.CharField(max_length=255)
+    object_name = models.CharField(max_length=255)
+
+    @staticmethod
+    def clean_dangling_objects():
+        objects_to_remove = [
+            DeleteObject(obj.object_name)
+            for obj in DefaultS3Video.objects.filter(thread=None, bucket=DEFAULT_S3_BUCKET)
+        ]
+        log.info(f"Removing {len(objects_to_remove)} items in S3 server")
+        errs = create_default_minio_client().remove_objects(
+            DEFAULT_S3_BUCKET,
+            objects_to_remove
+        )
+        for err in errs:
+            log.warning(f"Error while removing object: {err}")
+        DefaultS3Video.objects.filter(thread=None).delete()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['thread'])
+        ]
+        abstract = True
+        ordering = ["order", "id"]
+
 
 # NOVEL THREAD RELATED
+class NovelThread(models.Model, ISearchable):
+
+    @classmethod
+    def get_schema(cls) -> Schema:
+        return Schema(
+            id=ID(unique=True, stored=True),
+            title=TEXT(analyzer=get_analyzer()),
+            full_text=TEXT(analyzer=get_analyzer()),
+        )
+
+    @classmethod
+    def get_index_name(cls) -> str:
+        return "videothread"
+
+    def to_fields(self) -> Dict[str, Any]:
+        return {
+            "id": str(self.id),
+            # TODO read data from S3 as full_text
+            "title": clean_up_chinese_str(self.title),
+        }
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+
+    # MUST save in default bucket
+    object_name = models.CharField(max_length=255)
+
+    data = models.TextField(default="{}")
+
+    owner = models.ForeignKey(
+        "auth.User",
+        related_name="video_thread",
+        on_delete=models.SET_NULL,
+        null=True
+    )
