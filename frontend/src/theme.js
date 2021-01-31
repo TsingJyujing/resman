24cd5f948bf293d05d991a347b6cdf6afeba8925
Yifan Yuan <nigel434@gmail.com>
2021-01-31 22:45:12 +0900

    Using color from pornhub

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,11 +1,13 @@
-import { red } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import {createMuiTheme} from '@material-ui/core/styles';
 
 // A custom theme for this app
 const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-  },
+    palette: {
+        type: 'dark',
+        primary: {
+            main: "#ffa31a"
+        }
+    },
 });
 
 
