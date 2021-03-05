e23a6c937cbd3b0b871e366c0aab32d1c77b69e0
Yifan Yuan <nigel434@gmail.com>
2021-03-06 00:59:38 +0900

    Improve line change of description

--- frontend/src/DescriptionBlock.jsx
+++ frontend/src/DescriptionBlock.jsx
@@ -1,10 +1,11 @@
 import Typography from "@material-ui/core/Typography";
 import React from "react";
+import {RegExpReplace} from "./Utility";
 
 export default function DescriptionBlock({text, variant = "body1"}) {
     return (
         <Typography gutterBottom variant={variant} gutterBottom>{
-            text.split("\n").flatMap(line => {
+            text.split(/(\n|\s)+/).flatMap(line => {
                 return [line, <br/>]
             })
         }</Typography>
