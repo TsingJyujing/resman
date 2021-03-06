9619cdcd5526374f1160b02962dfbacee51ee8f5
Yifan Yuan <nigel434@gmail.com>
2021-03-06 12:48:36 +0900

    Remove useless code

--- frontend/src/DescriptionBlock.jsx
+++ frontend/src/DescriptionBlock.jsx
@@ -1,10 +1,9 @@
 import Typography from "@material-ui/core/Typography";
 import React from "react";
-import {RegExpReplace} from "./Utility";
 
 export default function DescriptionBlock({text, variant = "body1"}) {
     return (
-        <Typography gutterBottom variant={variant} gutterBottom>{
+        <Typography gutterBottom variant={variant}>{
             text.split(/(\n|\s)+/).flatMap(line => {
                 return [line, <br/>]
             })
