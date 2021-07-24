776a8f628e533de005e1e946e3e9e75c2a2a6455
Yifan Yuan <nigel434@gmail.com>
2021-07-24 22:51:46 +0900

    Fix startup script

--- load_data.sh
+++ load_data.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 python manage.py migrate
-python manage.py runserver --noreload 0.0.0.0:8000
+python manage.py loaddata $1
--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 python manage.py migrate
-python manage.py loaddata $1
+python manage.py runserver --noreload -1.0.0.0:8000
